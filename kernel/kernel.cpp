bd1ddac0e32aefd825c5c0373198c1e679fabd27
I73v <i73v@pm.me>
2023-07-16 03:23:32 -0400

    removed c mem function

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -11,62 +11,6 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
     .revision = 0
 };
 
-// GCC and Clang reserve the right to generate calls to the following
-// 4 functions even if they are not directly called.
-// Implement them as the C specification mandates.
-// DO NOT remove or rename these functions, or stuff will eventually break!
-// They CAN be moved to a different .c file.
-
-void *memcpy(void *dest, const void *src, size_t n) {
-    uint8_t *pdest = reinterpret_cast<uint8_t *>(dest);
-    const uint8_t *psrc = reinterpret_cast<const uint8_t *>(src);
-
-    for (size_t i = 0; i < n; i++) {
-        pdest[i] = psrc[i];
-    }
-
-    return dest;
-}
-
-void *memset(void *s, int c, size_t n) {
-    uint8_t *p = reinterpret_cast<uint8_t *>(s);
-
-    for (size_t i = 0; i < n; i++) {
-        p[i] = static_cast<uint8_t>(c);
-    }
-
-    return s;
-}
-
-void *memmove(void *dest, const void *src, size_t n) {
-    uint8_t *pdest = reinterpret_cast<uint8_t *>(dest);
-    const uint8_t *psrc = reinterpret_cast<const uint8_t *>(src);
-
-    if (src > dest) {
-        for (size_t i = 0; i < n; i++) {
-            pdest[i] = psrc[i];
-        }
-    } else if (src < dest) {
-        for (size_t i = n; i > 0; i--) {
-            pdest[i-1] = psrc[i-1];
-        }
-    }
-
-    return dest;
-}
-
-int memcmp(const void *s1, const void *s2, size_t n) {
-    const uint8_t *p1 = reinterpret_cast<const uint8_t *>(s1);
-    const uint8_t *p2 = reinterpret_cast<const uint8_t *>(s2);
-
-    for (size_t i = 0; i < n; i++) {
-        if (p1[i] != p2[i]) {
-            return p1[i] < p2[i] ? -1 : 1;
-        }
-    }
-
-    return 0;
-}
 
 // Halt and catch fire function.
 static void hcf(void) {
