15dc2844bba8c3f3e84810cd4a6838da64104523
I73v <i73v@pm.me>
2023-07-16 13:46:27 -0400

    Added cxxabi initialization Added kernel class Moved hcf into arch

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,45 +1,28 @@
-#include <stdint.h>
-#include <stddef.h>
+#include <kernel.hpp>
+
 #include <limine.h>
 
-// The Limine requests can be placed anywhere, but it is important that
-// the compiler does not optimise them away, so, usually, they should
-// be made volatile or equivalent.
+#include <abi/cxxabi.hpp>
+#include <base_types.hpp>
+#include <boot/loader.hpp>
 
-static volatile struct limine_framebuffer_request framebuffer_request = {
-    .id = LIMINE_FRAMEBUFFER_REQUEST,
-    .revision = 0
-};
+namespace nos {
 
-
-// Halt and catch fire function.
-static void hcf(void) {
-    __asm__ ("cli");
-    for (;;) {
-        __asm__ ("hlt");
-    }
+void kernel::init()
+{
+    cxxabi::init();
 }
 
-// The following will be our kernel's entry point.
-// If renaming _start() to something else, make sure to change the
-// linker script accordingly.
-extern "C" void _start(void)
+void kernel::run()
 {
-    // Ensure we got a framebuffer.
-    if (framebuffer_request.response == NULL
-     || framebuffer_request.response->framebuffer_count < 1) {
-        hcf();
-    }
-
-    // Fetch the first framebuffer.
-    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+    limine_framebuffer* framebuffer = boot::loader::get_framebuffer_request().response->framebuffers[0];
 
     // Note: we assume the framebuffer model is RGB with 32-bit pixels.
-    for (size_t i = 0; i < 500; i++) {
-        uint32_t *fb_ptr = reinterpret_cast<uint32_t*>(framebuffer->address);
+    for (size_t i = 0; i < 500; i++)
+    {
+        u32_t* fb_ptr = reinterpret_cast<u32_t*>(framebuffer->address);
         fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
     }
-
-    // We're done, just hang...
-    hcf();
 }
+
+} // namespace nos
