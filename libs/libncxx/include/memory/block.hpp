488998acc2bfd80ca2d941f4f00b5125f9ffadf4
I73v <i73v@pm.me>
2023-07-19 23:41:11 -0400

    Added ConstBlock  * Implicit conversion from Block

--- libs/libncxx/include/memory/block.hpp
+++ libs/libncxx/include/memory/block.hpp
@@ -5,66 +5,84 @@
 
 namespace nos::memory {
 
+struct Block;
+struct ConstBlock;
+
 struct Block
 {
     void* pointer;
     size_t size;
 
-    bool operator==(const Block&) const = default;
+    constexpr bool operator==(const Block&) const = default;
+
+    constexpr operator ConstBlock() const;
+};
+
+struct ConstBlock
+{
+    const void* pointer;
+    size_t size;
+
+    constexpr bool operator==(const ConstBlock&) const = default;
 };
 
 inline constexpr Block nullblk{nullptr, 0};
 
-constexpr int compare(Block lhs, Block rhs);
-constexpr int compare(Block lhs, Block rhs, size_t size);
+int compare(ConstBlock lhs, ConstBlock rhs);
+int compare(ConstBlock lhs, ConstBlock rhs, size_t size);
 
-constexpr void copy(Block& destination, Block source);
-constexpr void copy(Block& destination, Block source, size_t size);
+void copy(Block destination, ConstBlock source);
+void copy(Block destination, ConstBlock source, size_t size);
 
-constexpr void move(Block& destination, Block source);
-constexpr void move(Block& destination, Block source, size_t size);
+void move(Block destination, ConstBlock source);
+void move(Block destination, ConstBlock source, size_t size);
 
-void set(Block& block, byte value);
-void set(Block& block, byte value, size_t size);
+void set(Block destination, byte value);
+void set(Block destination, byte value, size_t size);
 
-constexpr int compare(Block lhs, Block rhs)
+constexpr Block::operator ConstBlock() const
+{
+    return ConstBlock{pointer, size};
+}
+
+inline int compare(Block lhs, Block rhs)
 {
     return compare(lhs, rhs, min(lhs.size, rhs.size));
 }
 
-constexpr int compare(Block lhs, Block rhs, size_t size)
+inline int compare(Block lhs, Block rhs, size_t size)
 {
     return __builtin_memcmp(lhs.pointer, rhs.pointer, size);
 }
 
-constexpr void copy(Block& destination, Block source)
+inline void copy(Block destination, ConstBlock source)
 {
     copy(destination, source, min(destination.size, source.size));
 }
 
-constexpr void copy(Block& destination, Block source, size_t size)
+inline void copy(Block destination, ConstBlock source, size_t size)
 {
     __builtin_memcpy(destination.pointer, source.pointer, size);
 }
 
-constexpr void move(Block& destination, Block source)
+inline void move(Block destination, ConstBlock source)
 {
     move(destination, source, min(destination.size, source.size));
 }
 
-constexpr void move(Block& destination, Block source, size_t size)
+inline void move(Block destination, ConstBlock source, size_t size)
 {
     __builtin_memmove(destination.pointer, source.pointer, size);
 }
 
-inline void set(Block& block, byte value)
+inline void set(Block destination, byte value)
 {
-    set(block, value, block.size);
+    set(destination, value, destination.size);
 }
 
-inline void set(Block& block, byte value, size_t size)
+inline void set(Block destination, byte value, size_t size)
 {
-    __builtin_memset(block.pointer, static_cast<int>(size), static_cast<unsigned long>(value));
+    __builtin_memset(destination.pointer, static_cast<int>(size), static_cast<unsigned long>(value));
 }
 
 } // namespace nos::memory
