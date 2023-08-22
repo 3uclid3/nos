e9c4f88a8430c8160509e639dcf7be268f2f6b7c
I73v <i73v@pm.me>
2023-08-22 04:42:41 -0400

    Properly include format test

--- libs/ncxx/ncxx/string/format.test.cpp
+++ libs/ncxx/ncxx/string/format.test.cpp
@@ -4,13 +4,13 @@
 
 namespace NOS {
 
-TEST_CASE("formatTo - bool", "[String]")
+TEST_CASE("format - circling buffer", "[String]")
 {
     constexpr StringView expectedResult = "Hello World";
 
     const StringView result1 = format("Hello {}", "World");
-    const StringView result2 = format("Hello {}", "World");
-    const StringView result3 = format("Hello {}", "World");
+    const StringView result2 = format("{} World", "Hello");
+    const StringView result3 = format("Hel{}rld", "lo Wo");
 
     CHECK(result1 == expectedResult);
     CHECK(result2 == expectedResult);
