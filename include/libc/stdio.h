377894677d3c612275d2d56b3a31393cc11bfbd1
3uclid3 <3uclid3@pm.me>
2024-07-22 13:21:12 -0400

    Refactor stdio.h: Streamline printf-family declarations
    Revised the format specifiers for printf-family functions to use HEDLEY_PRINTF_FORMAT for consistency and eliminated unused macros and file stream-related declarations. Improved maintainability by centralizing format attributes.

--- include/libc/stdio.h
+++ include/libc/stdio.h
@@ -5,36 +5,21 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-#define ATTR_FORMAT(fmt_index, first_arg) __attribute__((format(printf, (fmt_index), (first_arg))))
 #define EOF (-1)
 
 HEDLEY_BEGIN_C_DECLS
 
-typedef size_t FILE;
+int printf(const char* format, ...) HEDLEY_PRINTF_FORMAT(1, 2);
+int vprintf(const char* format, va_list arg) HEDLEY_PRINTF_FORMAT(1, 0);
 
-extern FILE* stdout;
-extern FILE* stderr;
+int sprintf(char* str, const char* format, ...) HEDLEY_PRINTF_FORMAT(2, 3);
+int vsprintf(char* str, const char* format, va_list arg) HEDLEY_PRINTF_FORMAT(2, 0);
 
-int fflush(FILE* stream);
+int snprintf(char* str, size_t count, const char* format, ...) HEDLEY_PRINTF_FORMAT(3, 4);
+int vsnprintf(char* str, size_t count, const char* format, va_list arg) HEDLEY_PRINTF_FORMAT(3, 0);
 
-int fputc(char c, FILE* stream);
-int fputs(const char* str, FILE* stream);
-int fputws(const wchar_t* str, FILE* stream);
-
-int fprintf(FILE* stream, const char* format, ...);
-size_t fwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream);
-
-int printf(const char* format, ...) ATTR_FORMAT(1, 2);
-int vprintf(const char* format, va_list arg) ATTR_FORMAT(1, 0);
-
-int sprintf(char* str, const char* format, ...) ATTR_FORMAT(2, 3);
-int vsprintf(char* str, const char* format, va_list arg) ATTR_FORMAT(2, 0);
-
-int snprintf(char* str, size_t count, const char* format, ...) ATTR_FORMAT(3, 4);
-int vsnprintf(char* str, size_t count, const char* format, va_list arg) ATTR_FORMAT(3, 0);
-
-int vasprintf(char** str, const char* format, va_list arg) ATTR_FORMAT(2, 0);
-int asprintf(char** str, const char* format, ...) ATTR_FORMAT(2, 3);
+int vasprintf(char** str, const char* format, va_list arg) HEDLEY_PRINTF_FORMAT(2, 0);
+int asprintf(char** str, const char* format, ...) HEDLEY_PRINTF_FORMAT(2, 3);
 
 int fctprintf(void (*out)(char c, void* extra_arg), void* extra_arg, const char* format, ...);
 int vfctprintf(void (*out)(char c, void* extra_arg), void* extra_arg, const char* format, va_list arg);
